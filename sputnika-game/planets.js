a5d016a605b113c59dc418e9e9e482c095954eab
kaihojun <kaihojun@naver.com>
2024-05-11 16:28:59 +0900

    이미지 업데이트

--- sputnika-game/planets.js
+++ sputnika-game/planets.js
@@ -1,23 +1,43 @@
 const PLANETS = [
     {
         name: "0",
-        radius: 47 / 2,
+        radius: 33 / 2,
     },
     {
         name: "1",
-        radius: 62 / 2,
+        radius: 43 / 2,
     },
     {
         name: "2",
-        radius: 78 / 2,
+        radius: 61 / 2,
     },
     {
         name: "3",
-        radius: 103 / 2,
+        radius: 69 / 2,
     },
     {
         name: "4",
+        radius: 89 / 2,
+    },
+    {
+        name: "5",
+        radius: 114 / 2,
+    },
+    {
+        name: "6",
+        radius: 129 / 2,
+    },
+    {
+        name: "7",
         radius: 120 / 2,
+    },
+    {
+        name: "8",
+        radius: 140 / 2,
+    },
+    {
+        name: "9",
+        radius: 170 / 2,
     }
 ];
 
